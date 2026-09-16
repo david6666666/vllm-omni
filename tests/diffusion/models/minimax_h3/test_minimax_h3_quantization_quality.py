9bfef9606a72c5388fac81239055dff3ec31fecf
david6666666 <530634352@qq.com>
2026-09-16 02:58:52 +0000

    [Bugfix][MiniMax-H3] Trust remote code in FP8 quality test
    Signed-off-by: david6666666 <530634352@qq.com>

--- tests/diffusion/models/minimax_h3/test_minimax_h3_quantization_quality.py
+++ tests/diffusion/models/minimax_h3/test_minimax_h3_quantization_quality.py
@@ -190,6 +190,8 @@ def test_minimax_h3_quantization_quality(config: QualityTestConfig):
         # The fused BF16 baseline only fits on H100-80GB with encoder TP.
         "text_encoder_tp_size": 2,
         "vae_use_tiling": True,
+        # MiniMax H3's VAE is implemented by code shipped with the checkpoint.
+        "trust_remote_code": True,
     }
 
     omni_bl = Omni(**common_kwargs)
